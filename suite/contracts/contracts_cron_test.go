428219d877f12f9becc407a633d24ecf42197533
skudasov <f4hrenh9it@gmail.com>
2021-07-14 12:22:40 +0300

    cronjob

--- suite/contracts/contracts_cron_test.go
+++ suite/contracts/contracts_cron_test.go
@@ -1,16 +1,18 @@
 package suite
 
 import (
+	"github.com/avast/retry-go"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	"github.com/pkg/errors"
 	"github.com/smartcontractkit/integrations-framework/client"
 	"github.com/smartcontractkit/integrations-framework/contracts"
 	"github.com/smartcontractkit/integrations-framework/suite"
 	"github.com/smartcontractkit/integrations-framework/tools"
 )
 
-var _ = FDescribe("Flux monitor suite", func() {
+var _ = Describe("Cronjob suite", func() {
 	DescribeTable("use cron job", func(
 		initFunc client.BlockchainNetworkInit,
 		ocrOptions contracts.OffchainOptions,
@@ -22,13 +24,25 @@ var _ = FDescribe("Flux monitor suite", func() {
 
 		adapter := tools.NewExternalAdapter()
 
-		_, err = chainlinkNodes[0].CreateJob(&client.CronJobSpec{
+		job, err := chainlinkNodes[0].CreateJob(&client.CronJobSpec{
 			Schedule:          "CRON_TZ=UTC * * * * * *",
 			ObservationSource: client.ObservationSourceSpec(adapter.InsideDockerAddr + "/five"),
 		})
 		Expect(err).ShouldNot(HaveOccurred())
-		//time.Sleep(999*time.Second)
-		//chainlinkNodes[0].ReadJob(job.Data.ID)
+		err = retry.Do(func() error {
+			jobRuns, err := chainlinkNodes[0].ReadRunsForJob(job.Data.ID)
+			if err != nil {
+				return err
+			}
+			if len(jobRuns.Data) != 5 {
+				return errors.New("not all jobs are completed")
+			}
+			for _, jr := range jobRuns.Data {
+				Expect(jr.Attributes.Errors).Should(Equal([]interface{}{nil}))
+			}
+			return nil
+		})
+		Expect(err).ShouldNot(HaveOccurred())
 	},
 		Entry("on Ethereum Hardhat", client.NewHardhatNetwork, contracts.DefaultOffChainAggregatorOptions()),
 	)
